f2a78410244b98265ff41e4a4365b35a82f666ec
Syed Azlan <syedazlan5169@gmail.com>
2024-08-25 02:04:06 +0800

    setting local env for phone

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+//    server: {
+//        host: "192.168.0.115",
+//        port: 3000,
+//        hmr: {
+//            host: "192.168.0.115",
+//        },
+//    },
 });
