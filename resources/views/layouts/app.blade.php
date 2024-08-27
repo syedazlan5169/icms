a87ab5ddcf1072303816fe47d98aefabf1482fa7
Syed Azlan <syedazlan5169@gmail.com>
2024-08-28 04:19:07 +0800

    update transition effect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,38 +47,41 @@
                     <!-- Profile dropdown -->
                     <div x-data="{ open: false }" class="relative ml-3">
                       <div>
-                        <button @click="open = !open" type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                          <span class="absolute -inset-1.5"></span>
-                          <span class="sr-only">Open user menu</span>
-                          <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                        </button>
+                          <button @click="open = !open" type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                              <span class="absolute -inset-1.5"></span>
+                              <span class="sr-only">Open user menu</span>
+                              <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                          </button>
                       </div>
-        
-                      <!--
-                        Dropdown menu, show/hide based on menu state.
-        
-                        Entering: "transition ease-out duration-100"
-                          From: "transform opacity-0 scale-95"
-                          To: "transform opacity-100 scale-100"
-                        Leaving: "transition ease-in duration-75"
-                          From: "transform opacity-100 scale-100"
-                          To: "transform opacity-0 scale-95"
-                      -->
-                      <div x-show="open" @click.outside="open = false" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                        <!-- Active: "bg-gray-100", Not Active: "" -->
-                        <x-dropdown-link :href="route('profile.edit')">{{ __('Profile') }}</x-dropdown-link>
-                        <x-dropdown-link href="/setting">{{ __('Setting') }}</x-dropdown-link>
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
 
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
+                      <!-- Dropdown menu, show/hide based on menu state. -->
+                      <div x-show="open" 
+                          @click.outside="open = false" 
+                          x-transition:enter="transition ease-out duration-100" 
+                          x-transition:enter-start="transform opacity-0 scale-95" 
+                          x-transition:enter-end="transform opacity-100 scale-100" 
+                          x-transition:leave="transition ease-in duration-75" 
+                          x-transition:leave-start="transform opacity-100 scale-100" 
+                          x-transition:leave-end="transform opacity-0 scale-95" 
+                          class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" 
+                          role="menu" 
+                          aria-orientation="vertical" 
+                          aria-labelledby="user-menu-button" 
+                          tabindex="-1">
+                          
+                          <x-dropdown-link :href="route('profile.edit')">{{ __('Profile') }}</x-dropdown-link>
+                          <x-dropdown-link href="/setting">{{ __('Setting') }}</x-dropdown-link>
+                          <form method="POST" action="{{ route('logout') }}">
+                              @csrf
+                              <x-dropdown-link :href="route('logout')"
+                                  onclick="event.preventDefault();
+                                          this.closest('form').submit();">
+                                  {{ __('Log Out') }}
+                              </x-dropdown-link>
+                          </form>
                       </div>
                     </div>
+
                   </div>
                 </div>
                 <div class="-mr-2 flex md:hidden">
