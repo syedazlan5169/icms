6b8e48f0c4cf80a5944ca2ee754e857ba03ee409
Syed Azlan <syedazlan5169@gmail.com>
2024-09-10 12:46:22 +0800

    istall livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite('resources/css/app.css')
+        @livewireStyles
+
     </head>
     <body class="h-full">
       <div class="min-h-full">
@@ -181,5 +183,6 @@
             </div>
           </main>
         </div>
+        @livewireScripts
     </body>
 </html>
